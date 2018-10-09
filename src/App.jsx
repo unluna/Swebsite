b32cc9f43e666b472eb25766b298ff16ae013ca5
ShiYue <729769298@qq.com>
2018-10-09 21:40:36 +0800

    科协网站，架构，初步完成...

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/*
+* 根组件，主干路由
+* */
+import React, { Component } from 'react'
+import './public.scss'
+
+export default class App extends Component {
+  render () {
+    return (
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    )
+  }
+}
