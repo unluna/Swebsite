f07554d32d6ce8d926ee4f97044b31ca95a0b24c
ShiYue <729769298@qq.com>
2018-10-20 18:53:12 +0800

    ...

--- src/component/home/header/header.jsx
+++ src/component/home/header/header.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import './header.scss'
 
 export default class Header extends Component {
+
+  sent=()=>{
+    const url = `/v2/music/search?q={{qwer}}&start=1`
+    axios.get(url)
+    .then(response => {
+      console.log(response)
+    })
+  }
+
   render () {
     return (
       <div>
         <header>
-          <div className={'header-nav-btn'} />
+          <div className={'header-nav-btn'} onClick={this.sent}/>
           <section className={'header-wrapper'}>
             <div className={'header-logo'}>
               <div className={'header-logo-img'}/>
@@ -31,11 +41,38 @@ export default class Header extends Component {
         </header>
         <aside>
           <ul>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
+            <li>
+              <div className={''}/>
+              <h1>教师</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>前端</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>后台</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>美术</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>安全</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>宣传</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>游戏</h1>
+            </li>
+            <li>
+              <div/>
+              <h1>资源</h1>
+            </li>
           </ul>
         </aside>
       </div>
