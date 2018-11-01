b7bdcdee9218d4b0c21ffb4d8dac9b31408f88d9
ShiYue <729769298@qq.com>
2018-11-01 17:15:58 +0800

    主页初步完成

--- src/component/home/header/header.jsx
+++ src/component/home/header/header.jsx
@@ -11,10 +11,10 @@ export default class Header extends Component {
 
   onChangeNavTop = () => {
     let {aside} = this.state
-    if (aside.style.top === `-460px`) {
+    if (aside.style.top === `-380px` || aside.style.top === '') {
       aside.style.top = 60 + 'px'
     } else {
-      aside.style.top = -460 + 'px'
+      aside.style.top = -380 + 'px'
     }
   }
 
@@ -25,42 +25,29 @@ export default class Header extends Component {
 
   render () {
     return (
-      <div>
-        <header>
-          <div className={'header-nav-btn'} onClick={this.onChangeNavTop}/>
-          <section className={'header-wrapper'}>
-            <div className={'header-logo'}>
-              <div className={'header-logo-img'}/>
-              <h1>数据科协</h1>
+      <header className={'B_header'}>
+        <section className={'header-wrapper'}>
+          <div className={'header-logo'} onClick={this.onChangeNavTop}>
+            <div className={'header-logo-img'}/>
+            <h1>数据科协</h1>
+          </div>
+          <Navigation getNav={this.getNav} ChangeNavTop={this.onChangeNavTop}/>
+          <div className={'header-aside'}>
+            <div className={'header-write-wrapper'}>
+              <div className={'icon-blog'}/>
+              <h1>写博客</h1>
             </div>
-            <div className={'header-aside'}>
-              <div className={'header-write-wrapper'}>
-                <div className={'icon-blog'}/>
-                <h1>写博客</h1>
-              </div>
-              <div className={'header-write-wrapper'}>
-                <div className={'icon-upload2'}/>
-                <h1>发资源</h1>
-              </div>
-              <div className={'header-write-wrapper'}>
-                <div className={'icon-user'}/>
-                <h1>登录</h1>
-              </div>
+            <div className={'header-write-wrapper'}>
+              <div className={'icon-upload2'}/>
+              <h1>发资源</h1>
             </div>
-          </section>
-        </header>
-        <Navigation getNav={this.getNav}/>
-        {
-          /*<aside>
-          <ul>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
-          </ul>
-        </aside>*/}
-      </div>
+            <div className={'header-write-wrapper'}>
+              <div className={'icon-user'}/>
+              <h1>登录</h1>
+            </div>
+          </div>
+        </section>
+      </header>
     )
   }
 }
--- src/rooter/navigation/navigation.jsx
+++ src/rooter/navigation/navigation.jsx
@@ -15,49 +15,49 @@ export default class Navigation extends Component {
     return (
       <aside ref={aside => this.aside = aside}>
         <ul>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li className={'aside-fst-child'}>
               <div className={'icon-lock'}/>
               <h1>教师</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>宣传</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>前端</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>后台</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>UI</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>安全</h1>
             </li>
           </NavLink>
-          <NavLink to={homeContent}>
+          <NavLink to={homeContent} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>游戏</h1>
             </li>
           </NavLink>
-          <NavLink to={sourceHome}>
+          <NavLink to={sourceHome} onClick={this.props.ChangeNavTop}>
             <li>
               <div className={'icon-lock'}/>
               <h1>资源</h1>
