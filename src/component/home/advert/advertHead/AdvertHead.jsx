1f03b3438654b705e1cb3a352a6443f776a30851
calarence-kong <39523754+calarence-kong@users.noreply.github.com>
2018-11-07 20:10:13 +0800

    K

--- src/component/home/advert/advertHead/AdvertHead.jsx
+++ src/component/home/advert/advertHead/AdvertHead.jsx
@@ -7,8 +7,9 @@ export default class AdvertHead extends Component {
       <div className="AdvertHead">
         <div className="headBG">
           <div className="AdvertTitle">
-            <h2>数据科学与技术学院</h2>
-            <h3>宣传部
+            <h2 className={'previous'}>数据科学与技术学院</h2>
+            <h2 className={'last'}> 科技创新协会</h2>
+            <h3 >宣传部
             <div className={'AdverLlogo'}> </div>
             </h3>
           </div>
---
+++ src/component/home/source/sourceSendHome/K-SourceSendBox/SourceSendBox.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+// import SourceSendBoxCount from './sourceSendCount/SourceSendBoxCount'
+import './SourceSendBox.scss'
+
+export default class SourceSendBox extends React.Component {
+  state = {
+    inners: '',
+    innersNum: 0,
+  }
+  change = (event) => {
+    const {inners, innersNum} = this.state
+    this.setState({
+      inners: event.target.value,
+      innersNum: inners.toString().length,
+    })
+  }
+  /*componentDidMount () {
+    let textarea = (id) => document.getElementById(id)
+    let count = (id) => {
+      textarea(id).onchange = () => {
+        const idValue = textarea(id).value
+        let data1 = {
+          num: parseInt(idValue.length),
+        }
+        data1.dNum = 1000 - data1.num
+        this.setState = ({
+          num: data1.num,
+          dNum: data1.dNum
+        })
+        alert(this.setState.num + 'haha' + this.setState.dNum)
+      }
+    }
+ count('boxTextarea')
+  }*/
+  autoHeight = (obj) => {
+    // let el=obj;
+    setTimeout(
+      () => {obj.style.text.cssText = 'height' + obj.scrollHeight + 'px'}, 0
+    )
+  }
+
+  render () {
+    const {inners, innersNum} = this.state
+    if ({innersNum} > 1000) {
+      this.setState = {
+        innersNum: 1000,
+      }
+      alert(innersNum)
+    }
+    console.log(inners)
+    console.log(innersNum)
+    return (
+      <div className="box">
+        <div className="box-login">
+          <div className="box-textArea">
+            <div className="Boxtitle"><textarea
+              placeholder="请输入标题"/></div>
+            <div className="SourceSendText">
+              <textarea placeholder="发点啥？(少于1000字符)" id="boxTextarea" onChange={this.change} value={inners}
+                        maxLength={1000}/>
+            </div>
+          </div>
+          <div className={'Count'}><p>{innersNum}</p><p>/1000</p></div>
+          <div className="box-info">
+            <div className="box-operate">
+              <b>阿拉斯加上课</b>
+              <span className="box-operate-line">|</span>
+            </div>
+            <div className="box-commentBtn">
+              <button className="iconfonticon-information-delivery">
+                <div className="button"><span>发布</span></div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
