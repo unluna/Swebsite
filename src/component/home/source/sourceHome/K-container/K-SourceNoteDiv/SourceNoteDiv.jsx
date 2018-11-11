0bff17ae885b21693cb7af27cf627be87ce95d1a
calarence-kong <39523754+calarence-kong@users.noreply.github.com>
2018-11-11 09:32:55 +0800

    update

--- src/component/home/source/sourceHome/K-container/K-SourceNoteDiv/SourceNoteDiv.jsx
+++ src/component/home/source/sourceHome/K-container/K-SourceNoteDiv/SourceNoteDiv.jsx
@@ -10,9 +10,7 @@ export default class SourceNoteDiv extends Component {
             <div className="noteDiv">
               <h2 className="sourceName">{this.props.title}</h2>
               <div className="noteHeader">
-                <img
-                  src="//q1.qlogo.cn/g?b=qq&amp;k=69ZiaGbnRyupQTrvoWfRnsA&amp;s=40&amp;t=1510934400"
-                  alt="头像" width="36" height="36"/>
+                <div className={'img'}> </div>
                 <span className="sender">{this.props.name}</span> <span className="time">2018-09-09</span>
               </div>
               <div className="textOut">
