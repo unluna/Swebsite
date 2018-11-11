6d3130944fa2e57109fbb2a8c01e947b5d820e0c
calarence-kong <39523754+calarence-kong@users.noreply.github.com>
2018-11-11 12:34:18 +0800

    update home.jsx,rooTable

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import './home.scss'
 import Header from './header/header'
-import { homeContent, background, fontEnd, game, advertHome } from '../../rooter/rootTable'
-import {sourceR} from '../../rooter/rootTable'
+import { homeContent, background, fontEnd, game, advertHome,sourceR,members } from '../../rooter/rootTable'
 import HomeContent from './homeContent/homeContent'
 import SourceR from './source/sourceR'
 import Background from './background/background'
 import FontEnd from './font-end/font-end'
 import Game from './game/game'
 import AdvertHome from './advert/advertHome'
+import MembersHome from './members/MembersHome'
 export default class Home extends Component {
   render () {
     return (
@@ -22,6 +22,7 @@ export default class Home extends Component {
           <Route path={fontEnd} component={FontEnd}/>
           <Route path={game} component={Game}/>
           <Route path={advertHome} component={AdvertHome}/>
+          <Route path={members} component={MembersHome}/>
           <Redirect to={homeContent}/>
         </Switch>
       </div>
